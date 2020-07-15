3d021bd98ffdaf462961ef418ebf1ff400e28c08
Murad <muradkarammaev@gmail.com>
2020-07-15 10:55:47 +0300

    fix buffer size
    ChaCha20 is 256-bit cipher, but it outputs 512 bits per block

--- cc20.c
+++ cc20.c
@@ -17,8 +17,8 @@ int main(int argc, char *argv[static argc]) {
 
 int main_loop(FILE *input, FILE *output) {
     size_t nread;
-    uint8_t buf[32];
-    while ((nread = fread(buf, sizeof(uint8_t), 32, input)) > 0) {
+    uint8_t buf[64];
+    while ((nread = fread(buf, sizeof(uint8_t), 64, input)) > 0) {
         if (fwrite(buf, sizeof(uint8_t), nread, output) != nread) {
             return -1;
         }
