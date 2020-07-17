5274eb14d62a05d1bae64cd3490278a79e10aed6
Murad <muradkarammaev@gmail.com>
2020-07-17 07:06:37 +0300

    implement decryption
    use random nonce

--- cc20.c
+++ cc20.c
@@ -3,8 +3,8 @@
 int main(int argc, char *argv[static argc]) {
     struct config cfg;
     struct chacha20_state cc20;
-    uint8_t nonce[12] = {'c', 'h', 'a', 'c', 'h',
-                         'a', '2', '0', 20}; // TODO: remove hardcoded nonce
+    srand(clock() ^ time(NULL));
+    uint8_t nonce[12];
     if (parse_args(argc, argv, &cfg) != 0) {
         if (cfg.help) {
             print_help(&cfg, EXIT_FAILURE);
@@ -17,7 +17,16 @@ int main(int argc, char *argv[static argc]) {
         print_help(&cfg, EXIT_SUCCESS);
     }
     if (cfg.decrypt) {
-        die("Decryption is not implemented");
+        if (fread(nonce, sizeof(uint8_t), 12, cfg.input) != 12) {
+            die("Failed to read nonce");
+        }
+    } else {
+        for (size_t i = 0; i < 12; ++i) {
+            nonce[i] = rand();
+        }
+        if (fwrite(nonce, sizeof(uint8_t), 12, cfg.output) != 12) {
+            die("Failed to write nonce");
+        }
     }
     chacha20_init(&cc20, cfg.key, nonce);
     return (main_loop(cfg.input, cfg.output, &cc20) == 0) ? EXIT_SUCCESS
