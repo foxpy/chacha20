67ee9add7e1018f25632f4f510074c66b69d6110
Murad <muradkarammaev@gmail.com>
2021-01-26 22:47:46 +0300

    throw macros away and replace them with human readable function

--- chacha20.c
+++ chacha20.c
@@ -1,24 +1,19 @@
 #include "cc20.h"
 
-#define PLUS(a, b) ((a) + (b))
-#define XOR(a, b) ((a) ^ (b))
-#define ROTL32(a, n) (((a) << (n)) | ((a) >> (32 - (n))))
-
-#define QROUND(a, b, c, d)                                                     \
-    {                                                                          \
-        (a) = PLUS((a), (b));                                                  \
-        (d) = XOR((d), (a));                                                   \
-        (d) = ROTL32((d), 16);                                                 \
-        (c) = PLUS((c), (d));                                                  \
-        (b) = XOR((b), (c));                                                   \
-        (b) = ROTL32((b), 12);                                                 \
-        (a) = PLUS((a), (b));                                                  \
-        (d) = XOR((d), (a));                                                   \
-        (d) = ROTL32((d), 8);                                                  \
-        (c) = PLUS((c), (d));                                                  \
-        (b) = XOR((b), (c));                                                   \
-        (b) = ROTL32((b), 7);                                                  \
-    }
+static void qround(uint32_t* a, uint32_t* b, uint32_t* c, uint32_t* d) {
+    *a += *b;
+    *d ^= *a;
+    *d = qc_rotl32(*d, 16);
+    *c += *d;
+    *b ^= *c;
+    *b = qc_rotl32(*b, 12);
+    *a += *b;
+    *d ^= *a;
+    *d = qc_rotl32(*d, 8);
+    *c += *d;
+    *b ^= *c;
+    *b = qc_rotl32(*b, 7);
+}
 
 void chacha20_init(struct chacha20_state *state, uint8_t key[static 32],
                    uint8_t nonce[static 12]) {
@@ -37,14 +32,14 @@ void chacha20_next(struct chacha20_state *state, uint8_t buf[static 64]) {
     uint32_t *s = state->copy;
     uint32_t *output = (uint32_t *)buf;
     for (size_t i = 0; i < 10; ++i) {
-        QROUND(s[0], s[4], s[8], s[12]);
-        QROUND(s[1], s[5], s[9], s[13]);
-        QROUND(s[2], s[6], s[10], s[14]);
-        QROUND(s[3], s[7], s[11], s[15]);
-        QROUND(s[0], s[5], s[10], s[15]);
-        QROUND(s[1], s[6], s[11], s[12]);
-        QROUND(s[2], s[7], s[8], s[13]);
-        QROUND(s[3], s[4], s[9], s[14]);
+        qround(&s[0], &s[4], &s[8], &s[12]);
+        qround(&s[1], &s[5], &s[9], &s[13]);
+        qround(&s[2], &s[6], &s[10], &s[14]);
+        qround(&s[3], &s[7], &s[11], &s[15]);
+        qround(&s[0], &s[5], &s[10], &s[15]);
+        qround(&s[1], &s[6], &s[11], &s[12]);
+        qround(&s[2], &s[7], &s[8], &s[13]);
+        qround(&s[3], &s[4], &s[9], &s[14]);
     }
     for (size_t i = 0; i < 16; ++i) {
         state->copy[i] += state->state[i];
