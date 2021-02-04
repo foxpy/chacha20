1dea8d4ff5604966539be6a91675e44c623d00b9
Murad <muradkarammaev@gmail.com>
2021-02-04 13:10:53 +0300

    reformat and refactor all code in project

--- cmd/cc20.c
+++ cmd/cc20.c
@@ -10,8 +10,8 @@
 
 typedef struct cfg {
     bool decrypt;
-    FILE *input;
-    FILE *output;
+    FILE* input;
+    FILE* output;
     uint8_t key[32];
 } cfg;
 
@@ -25,11 +25,39 @@ static void free_config(cfg* c) {
     memset(c, 0, sizeof(cfg));
 }
 
-static qc_result parse_arguments(cfg *c, int argc, char *argv[], qc_err *err) {
-    char const *input_file_str;
-    char const *output_file_str;
-    char const *key_str;
-    qc_args *args = qc_args_new();
+static qc_result open_file(char const* path, char const* mode, FILE* backup, FILE** dst, qc_err* err) {
+    errno = 0;
+    if (strcmp(path, "-") == 0) {
+        *dst = backup;
+    } else if ((*dst = fopen(path, mode)) == NULL) {
+        qc_err_set(err, "Failed to open input file \"%s\": %s", path, strerror(errno));
+        return QC_FAILURE;
+    }
+    return QC_SUCCESS;
+}
+
+static qc_result parse_key(uint8_t dst[32], char const* src, qc_err* err) {
+    uint8_t* decoded;
+    ptrdiff_t len = qc_hexstr_to_bytes(src, &decoded);
+    if (len < 0) {
+        qc_err_set(err, "Failed to parse \"%s\" as valid ChaCha20 key", src);
+        return QC_FAILURE;
+    } else if (len != 32) {
+        qc_err_set(err, "ChaCha20 key should be %d bits long, but provided key is %td bits long", 256, len * 8);
+        free(decoded);
+        return QC_FAILURE;
+    } else {
+        memmove(dst, decoded, 32);
+        free(decoded);
+        return QC_SUCCESS;
+    }
+}
+
+static qc_result parse_arguments(cfg* c, int argc, char* argv[], qc_err* err) {
+    char const* input_file_str;
+    char const* output_file_str;
+    char const* key_str;
+    qc_args* args = qc_args_new();
     qc_args_flag(args, 'd', "decrypt", &c->decrypt, "decrypt data");
     qc_args_string_default(args, "input", "-", &input_file_str, "input file, stdin if omitted");
     qc_args_string_default(args, "output", "-", &output_file_str, "output file, stdout if omitted");
@@ -37,49 +65,16 @@ static qc_result parse_arguments(cfg *c, int argc, char *argv[], qc_err *err) {
                    "256-bit little endian key in hexadecimal format "
                    "(example: 0x000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f)");
     memset(c, 0, sizeof(cfg));
-    if (qc_args_parse(args, argc, argv, err) != QC_SUCCESS) {
-        goto error;
+    if (qc_args_parse(args, argc, argv, err) == QC_FAILURE || parse_key(c->key, key_str, err) == QC_FAILURE ||
+        open_file(input_file_str, "rb", stdin, &c->input, err) == QC_FAILURE ||
+        open_file(output_file_str, "wb", stdout, &c->output, err) == QC_FAILURE) {
+        qc_args_free(args);
+        free_config(c);
+        return QC_FAILURE;
     } else {
-        {
-            uint8_t *decoded;
-            ptrdiff_t len = qc_hexstr_to_bytes(key_str, &decoded);
-            if (len < 0) {
-                qc_err_set(err, "Failed to parse \"%s\" as valid ChaCha20 key", key_str);
-                goto error;
-            } else if (len != 32) {
-                qc_err_set(err, "ChaCha20 key should be %d bits long, but provided key is %td bits long", 256,
-                           len * 8);
-                goto error;
-            } else {
-                memmove(c->key, decoded, 32);
-                free(decoded);
-            }
-        }
-        {
-            errno = 0;
-            if (strcmp(input_file_str, "-") == 0) {
-                c->input = stdin;
-            } else if ((c->input = fopen(input_file_str, "rb")) == NULL) {
-                qc_err_set(err, "Failed to open input file \"%s\": %s", input_file_str, strerror(errno));
-                goto error;
-            }
-        }
-        {
-            errno = 0;
-            if (strcmp(output_file_str, "-") == 0) {
-                c->output = stdout;
-            } else if ((c->output = fopen(output_file_str, "wb")) == NULL) {
-                qc_err_set(err, "Failed to open output file \"%s\": %s", output_file_str, strerror(errno));
-                goto error;
-            }
-        }
         qc_args_free(args);
         return QC_SUCCESS;
     }
-error:
-    qc_args_free(args);
-    free_config(c);
-    return QC_FAILURE;
 }
 
 static qc_result main_loop(chacha20_state* cc20, FILE* dst, FILE* src, qc_err* err) {
@@ -96,8 +91,8 @@ static qc_result main_loop(chacha20_state* cc20, FILE* dst, FILE* src, qc_err* e
     return QC_SUCCESS;
 }
 
-int main(int argc, char *argv[]) {
-    qc_err *err = qc_err_new();
+int main(int argc, char* argv[]) {
+    qc_err* err = qc_err_new();
     cfg config;
     if (parse_arguments(&config, argc, argv, err) != QC_SUCCESS) {
         qc_err_fatal(err, "Failed to parse command line arguments");
