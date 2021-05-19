e70e5c1b974bc52da2eaadc137d9cff9cafb999f
Murad <muradkarammaev@gmail.com>
2021-05-19 14:19:32 +0300

    add poorly tested multiplication

--- src/poly1305/tests/poly1305-uint288_t-test.c
+++ src/poly1305/tests/poly1305-uint288_t-test.c
@@ -13,35 +13,60 @@ static void serialization(qc_rnd* rnd) {
     qc_assert(memcmp(original, copy, 36) == 0, "uint288_t (de)serialization is broken");
 }
 
-void addition_iteration(uint8_t const a[36], uint8_t const b[36], uint8_t const expected[36]) {
+void iteration(uint8_t const a[36], uint8_t const b[36], uint8_t const expected[36],
+               void (f)(uint288_t*, uint288_t const*), char const* f_name) {
     uint288_t u288_a, u288_b;
     poly1305_uint288_t_from_le_bytes(&u288_a, a);
     poly1305_uint288_t_from_le_bytes(&u288_b, b);
-    poly1305_uint288_t_add(&u288_a, &u288_b);
+    f(&u288_a, &u288_b);
     uint8_t actual[36] = {0};
     poly1305_uint288_t_to_le_bytes(actual, &u288_a);
-    qc_assert(memcmp(actual, expected, 36) == 0, "uint288_t addition does not produce expected results");
+    qc_assert(memcmp(actual, expected, 36) == 0, "%s does not produce expected results", f_name);
 }
+
+#define ITER(a, b, expected, f) iteration(a, b, expected, f, #f)
 
 void addition(void) {
     // clang-format off
-    addition_iteration(
-        (uint8_t const[36]){15, 0},
-        (uint8_t const[36]){18, 0},
-        (uint8_t const[36]){33, 0}
+    ITER(
+            ((uint8_t const[36]){15, 0}),
+            ((uint8_t const[36]){18, 0}),
+            ((uint8_t const[36]){33, 0}),
+            poly1305_uint288_t_add
     );
-    addition_iteration(
-            (uint8_t const[36]){27, 6,  0},
-            (uint8_t const[36]){34, 18, 0},
-            (uint8_t const[36]){61, 24, 0}
+    ITER(
+            ((uint8_t const[36]){27, 6,  0}),
+            ((uint8_t const[36]){34, 18, 0}),
+            ((uint8_t const[36]){61, 24, 0}),
+            poly1305_uint288_t_add
     );
-    addition_iteration(
-            (uint8_t const[36]){225, 139, 100, 16, 234, 221, 76, 113, 251, 0, 0},
-            (uint8_t const[36]){8,   251, 184, 16, 89,  79,  13, 181, 108, 0, 0},
-            (uint8_t const[36]){233, 134, 29,  33, 67,  45,  90, 38,  104, 1, 0}
+    ITER(
+            ((uint8_t const[36]){225, 139, 100, 16, 234, 221, 76, 113, 251, 0, 0}),
+            ((uint8_t const[36]){8,   251, 184, 16, 89,  79,  13, 181, 108, 0, 0}),
+            ((uint8_t const[36]){233, 134, 29,  33, 67,  45,  90, 38,  104, 1, 0}),
+            poly1305_uint288_t_add
     );
     // clang-format on
 }
+
+void multiplication(void) {
+    // clang-format off
+    ITER(
+            ((uint8_t const[36]){2, 0}),
+            ((uint8_t const[36]){3, 0}),
+            ((uint8_t const[36]){6, 0}),
+            poly1305_uint288_t_mul
+    );
+    ITER(
+            ((uint8_t const[36]){24,    1, 0, 0}),
+            ((uint8_t const[36]){38,    6, 0, 0}),
+            ((uint8_t const[36]){144, 185, 6, 0}),
+            poly1305_uint288_t_mul
+    );
+    // clang-format on
+}
+
+#undef ITER
 
 int main(void) {
     qc_rnd rnd;
@@ -51,5 +76,6 @@ int main(void) {
         serialization(&rnd);
     }
     addition();
+    multiplication();
     qc_err_free(err);
 }
--- src/poly1305/uint288_t.c
+++ src/poly1305/uint288_t.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
+#include <assert.h>
 #include <qc/endian.h>
 #include "poly1305_impl.h"
 
@@ -25,5 +27,40 @@ void poly1305_uint288_t_add(uint288_t* a, uint288_t const* b) {
         carry = (uint32_t)(accumulator >> 32);
         accumulator &= UINT32_MAX;
         a->u[i] = (uint32_t) accumulator;
+    }
+}
+
+static void poly1305_impl_uint288_t_mul_scalar(uint288_t* a, uint32_t s) {
+    uint32_t carry = 0;
+    for (size_t i = 0; i < 9; ++i) {
+        uint64_t accumulator = a->u[i];
+        accumulator *= s;
+        accumulator += carry;
+        carry = (uint32_t)(accumulator >> 32);
+        accumulator &= UINT32_MAX;
+        a->u[i] = (uint32_t) accumulator;
+    }
+}
+
+static void poly1305_impl_uint288_t_mul_shift_right(uint288_t* a, size_t n) {
+    assert(n < 9);
+    for (size_t i = n; i < 9; ++i) {
+        a->u[i] = a->u[i-n];
+    }
+    for (size_t i = 0; i < n; ++i) {
+        a->u[i] = 0;
+    }
+}
+
+void poly1305_uint288_t_mul(uint288_t* a, uint288_t const* b) {
+    uint288_t m[9];
+    for (size_t i = 0; i < 9; ++i) {
+        memcpy(&m[i], a, sizeof(uint288_t));
+        poly1305_impl_uint288_t_mul_scalar(&m[i], b->u[i]);
+        poly1305_impl_uint288_t_mul_shift_right(&m[i], i);
+    }
+    memset(a, 0, sizeof(uint288_t));
+    for (size_t i = 0; i < 9; ++i) {
+        poly1305_uint288_t_add(a, &m[i]);
     }
 }
